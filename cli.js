fc79412c77fcb714c69943ca01c96ff7d6e1be21
lmachens <leon.machens@googlemail.com>
2020-08-24 10:29:38 +0200

    Replace readline with inquirer

--- cli.js
+++ cli.js
@@ -1,21 +1,19 @@
-// import readline from 'readline';
-const readline = require("readline");
+const inquirer = require("inquirer");
 
-const interface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+const questions = [
+  {
+    type: "password",
+    name: "password",
+    message: "What's your password?",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What's your name?",
+  },
+];
 
-interface.question("What's your password?", password => {
-    console.log(`Your password is ${password}`);
-    if (password !== 'abc') {
-        console.log('Wrong password');
-        interface.close();
-        return;
-    }
-
-    interface.question("What's your name?", name => {
-        console.log(`Your name is ${name}`);
-        interface.close();
-    })
-})
+inquirer.prompt(questions).then((answers) => {
+  console.log(`Your password is ${answers.password}!`);
+  console.log(`Your name is ${answers.name}!`);
+});
