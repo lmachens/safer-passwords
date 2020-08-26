56d2e6f3ac59298fffb15130b81614f64e4ca498
lmachens <leon.machens@googlemail.com>
2020-08-26 11:19:36 +0200

    Encryt and decrypt passwords

--- cli.js
+++ cli.js
@@ -6,6 +6,7 @@ const {
   CHOICE_SET,
 } = require("./lib/questions");
 const { readPassword, writePassword } = require("./lib/passwords");
+const { encrypt, decrypt } = require("./lib/crypto");
 
 async function main() {
   const { masterPassword, action } = await askStartQuestions();
@@ -16,7 +17,9 @@ async function main() {
       console.log("Now Get a password");
       const { key } = await askGetPasswordQuestions();
       try {
-        const password = await readPassword(key);
+        const encryptedPassword = await readPassword(key);
+        const password = decrypt(encryptedPassword, masterPassword);
+
         console.log(`Your ${key} password is ${password}`);
       } catch (error) {
         console.error("Something went wrong 😑");
@@ -24,9 +27,15 @@ async function main() {
       }
     } else if (action === CHOICE_SET) {
       console.log("Now Set a password");
-      const { key, password } = await askSetPasswordQuestions();
-      await writePassword(key, password);
-      console.log(`New Password set`);
+      try {
+        const { key, password } = await askSetPasswordQuestions();
+        const encryptedPassword = encrypt(password, masterPassword);
+        await writePassword(key, encryptedPassword);
+        console.log(`New Password set`);
+      } catch (error) {
+        console.error("Something went wrong 😑");
+        // What to do now?
+      }
     }
   } else {
     console.log("Master Password is incorrect!");
