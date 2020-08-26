c1119a5051df2499fef4e759b8cba49b1af647af
lmachens <leon.machens@googlemail.com>
2020-08-26 14:34:21 +0200

    Hash master password

--- cli.js
+++ cli.js
@@ -12,20 +12,21 @@ const {
   readMasterPassword,
   writeMasterPassword,
 } = require("./lib/passwords");
-const { encrypt, decrypt } = require("./lib/crypto");
+const { encrypt, decrypt, createHash, verifyHash } = require("./lib/crypto");
 
 async function main() {
   const originalMasterPassword = await readMasterPassword();
   if (!originalMasterPassword) {
     const { newMasterPassword } = await askForNewMasterPassword();
-    await writeMasterPassword(newMasterPassword);
+    const hashedMasterPassword = createHash(newMasterPassword);
+    await writeMasterPassword(hashedMasterPassword);
     console.log("Master Password set!");
     return;
   }
 
   const { masterPassword, action } = await askStartQuestions();
 
-  if (masterPassword !== originalMasterPassword) {
+  if (!verifyHash(masterPassword, originalMasterPassword)) {
     console.log("Master Password is incorrect!");
     return;
   }
