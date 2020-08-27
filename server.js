e0626deae2d13bb5b858a7575c78f68d5ab42169
marwinburesch <marwin@posteo.de>
2020-08-27 15:18:52 +0200

    Add GET endpoint to /api/passwords/

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const { MongoClient } = require("mongodb");
-const { writePassword } = require("./lib/passwords");
-const { encrypt } = require("./lib/crypto");
+const { writePassword, readPassword } = require("./lib/passwords");
+const { encrypt, decrypt } = require("./lib/crypto");
 const bodyParser = require("body-parser");
 
 const client = new MongoClient(process.env.MONGO_URI);
@@ -17,6 +17,13 @@ async function main() {
   await client.connect();
   const database = client.db(process.env.MONGO_DB_NAME);
   const masterPassword = process.env.MASTER_PASSWORD;
+
+  app.get("/api/passwords/:name", async (request, response) => {
+    const { name } = request.params;
+    const password = await readPassword(name, database);
+    const decryptedPassword = decrypt(password, masterPassword);
+    response.send(decryptedPassword);
+  });
 
   app.post("/api/passwords", async (request, response) => {
     console.log("POST on /api/passwords");
