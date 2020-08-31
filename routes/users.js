95284012fe168a8b367bca3dc7d06d7be2c69632
lmachens <leon.machens@googlemail.com>
2020-08-31 15:16:10 +0200

    Find user by email and password

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,26 @@ const express = require("express");
 function createUsersRouter(database) {
   const router = express.Router();
 
+  const usersCollection = database.collection("users");
+
   router.post("/login", async (request, response) => {
-    const { email, password } = request.body;
-    console.log(email, password);
-    response.send("Logged in");
+    try {
+      const { email, password } = request.body;
+      const user = await usersCollection.findOne({
+        email,
+        password,
+      });
+      if (!user) {
+        response.status(401).send("Wrong email or password");
+        return;
+      }
+
+      console.log(user);
+      response.send("Logged in");
+    } catch (error) {
+      console.error(error);
+      response.status(500).send(error.message);
+    }
   });
 
   return router;
