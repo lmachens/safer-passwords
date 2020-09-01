9cc0bfd9516cfaae44bd6132a0094b1cbe2bedc0
lmachens <leon.machens@googlemail.com>
2020-09-01 16:20:50 +0200

    Add proxy and fetch from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+async function login() {
+  try {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "l@machens.koeln",
+        password: "123",
+      }),
+    });
+    const result = await response.text();
+    alert(result);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+async function fetchPassword(name) {
+  try {
+    const response = await fetch(`/api/passwords/${name}`);
+    const result = await response.text();
+    alert(result);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
 function App() {
+  const [passwordName, setPasswordName] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={login}>Login</button>
+      <label>
+        Password-Name{" "}
+        <input
+          value={passwordName}
+          onChange={(event) => setPasswordName(event.target.value)}
+        />
+      </label>
+      <button onClick={() => fetchPassword(passwordName)}>Get Password</button>
     </div>
   );
 }
