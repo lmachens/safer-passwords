71fffa46725032b234a610a500613e6ce27d864a
lmachens <leon.machens@googlemail.com>
2020-09-01 12:28:16 +0200

    Patch passwords

--- routes/passwords.js
+++ routes/passwords.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { readPassword, writePassword } = require("../lib/passwords");
+const {
+  readPassword,
+  writePassword,
+  updatePassword,
+} = require("../lib/passwords");
 const { decrypt, encrypt } = require("../lib/crypto");
 const jwt = require("jsonwebtoken");
 
@@ -30,6 +34,50 @@ function createPasswordsRouter(database, masterPassword) {
       const password = decrypt(encryptedPassword, masterPassword);
 
       response.send(password);
+    } catch (error) {
+      console.error(error);
+      response.status(500).send(error.message);
+    }
+  });
+
+  router.patch("/:name", async (request, response) => {
+    try {
+      const { name } = request.params;
+      const { name: newName, value: newValue } = request.body;
+
+      const existingPassword = await readPassword(name, database);
+      if (!existingPassword) {
+        response.status(404).send("Password doesn't exists");
+        return;
+      }
+
+      // if (newName && newValue) {
+      //   updatePassword(
+      //     newName,
+      //     encrypt(newValue, masterPassword),
+      //     database
+      //   );
+      // } else if (newName) {
+      //   updatePassword(
+      //     newName,
+      //     existingPassword,
+      //     database
+      //   );
+      // } else if (newValue) {
+      //   updatePassword(
+      //     name,
+      //     encrypt(newValue, masterPassword),
+      //     database
+      //   );
+      // }
+
+      await updatePassword(
+        newName || name,
+        newValue ? encrypt(newValue, masterPassword) : existingPassword,
+        database
+      );
+
+      response.status(200).send("Password updated");
     } catch (error) {
       console.error(error);
       response.status(500).send(error.message);
